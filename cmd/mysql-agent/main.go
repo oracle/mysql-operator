abdb3d590df350131b9078be59be34905c59b8ef
Andrew Pryde <andrew@rocketpod.co.uk>
2018-04-13 16:15:58 +0100

    Fix logging before flag.Parse (#81)

--- cmd/mysql-agent/main.go
+++ cmd/mysql-agent/main.go
@@ -15,12 +15,14 @@
 package main
 
 import (
+	goflag "flag"
 	"fmt"
 	"os"
 
-	flags "k8s.io/apiserver/pkg/util/flag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 
+	"github.com/golang/glog"
 	"github.com/spf13/pflag"
 
 	"github.com/oracle/mysql-operator/cmd/mysql-agent/app"
@@ -30,12 +32,21 @@ import (
 
 func main() {
 	fmt.Fprintf(os.Stderr, "Starting mysql-agent version %s\n", version.GetBuildVersion())
-	opts := options.NewMySQLAgentOpts()
-	opts.AddFlags(pflag.CommandLine)
 
-	flags.InitFlags()
+	opts := options.NewMySQLAgentOpts()
+
+	opts.AddFlags(pflag.CommandLine)
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	pflag.Parse()
+	goflag.CommandLine.Parse([]string{})
+
 	logs.InitLogs()
 	defer logs.FlushLogs()
+
+	pflag.VisitAll(func(flag *pflag.Flag) {
+		glog.V(2).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
 
 	if err := app.Run(opts); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
