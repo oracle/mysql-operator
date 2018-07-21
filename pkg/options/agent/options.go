80a01a964a1df81011ad9b834e34ea4474b2f91a
huanwei <huan@harmonycloud.cn>
2018-07-21 08:30:55 +0800

    tiny refactor code structure
    Signed-off-by: huanwei <huan@harmonycloud.cn>

---
+++ pkg/options/agent/options.go
@@ -0,0 +1,103 @@
+// Copyright 2018 Oracle and/or its affiliates. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package agent
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/spf13/pflag"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// DefaultMySQLAgentHeathcheckPort is the port on which the mysql-agent's
+	// healthcheck service runs on.
+	DefaultMySQLAgentHeathcheckPort int32 = 10512
+)
+
+// MySQLAgentOpts holds the configuration options required to
+// run the backup controller.
+type MySQLAgentOpts struct {
+	// HealthcheckPort is the port on which the mysql-agent healthcheck http
+	// service runs on.
+	HealthcheckPort int32
+
+	// Address is the IP address to serve the backon. Set to 0.0.0.0 to listen
+	// on all interfaces.
+	Address string
+
+	// Namespace is the namespace in which the backup controller (and is
+	// associated Cluster) are running.
+	Namespace string
+	// ClusterName is the name of the Cluster the backup controller
+	// is responsible for.
+	ClusterName string
+	// Hostname of the pod the backup operator is running in.
+	Hostname string
+
+	// minResyncPeriod is the resync period in reflectors; will be random
+	// between minResyncPeriod and 2*minResyncPeriod.
+	MinResyncPeriod metav1.Duration
+}
+
+// NewMySQLAgentOpts instantiates a new default
+// MySQLAgentOpts getting values from the env where possible.
+func NewMySQLAgentOpts() *MySQLAgentOpts {
+	hostname, err := os.Hostname()
+	if err != nil {
+		glog.Fatalf("Failed to get the hostname: %v", err)
+	}
+	namespace := os.Getenv("POD_NAMESPACE")
+	clusterName := os.Getenv("MYSQL_CLUSTER_NAME")
+	return &MySQLAgentOpts{
+		HealthcheckPort: DefaultMySQLAgentHeathcheckPort,
+		Address:         "0.0.0.0",
+		Namespace:       namespace,
+		ClusterName:     clusterName,
+		Hostname:        hostname,
+		MinResyncPeriod: metav1.Duration{Duration: 12 * time.Hour},
+	}
+}
+
+// AddFlags adds the mysql-agent flags to a given FlagSet.
+func (s *MySQLAgentOpts) AddFlags(fs *pflag.FlagSet) *pflag.FlagSet {
+	fs.Int32Var(&s.HealthcheckPort, "healthcheck-port", s.HealthcheckPort, "The port that the mysql-agent's healthcheck http service runs on.")
+	fs.StringVar(&s.Address, "address", s.Address, "The IP address to serve the mysql-agent's http service on (set to 0.0.0.0 for all interfaces).")
+
+	fs.StringVar(&s.Namespace, "namespace", s.Namespace, "The namespace to run in. Must be the same namespace as the associated MySQL cluster.")
+	fs.StringVar(&s.ClusterName, "cluster-name", s.ClusterName, "The name of the MySQL cluster the mysql-agent is responsible for.")
+	fs.StringVar(&s.Hostname, "hostname", s.Hostname, "The hostname of the pod the mysql-agent is running in.")
+	fs.DurationVar(&s.MinResyncPeriod.Duration, "min-resync-period", s.MinResyncPeriod.Duration, "The resync period in reflectors will be random between MinResyncPeriod and 2*MinResyncPeriod.")
+
+	return fs
+}
+
+// Validate checks that the required config options have been set.
+func (s *MySQLAgentOpts) Validate() error {
+	if len(s.Namespace) == 0 {
+		return fmt.Errorf("must set --namespace or $POD_NAMESPACE")
+	}
+	if len(s.ClusterName) == 0 {
+		return fmt.Errorf("must set --cluster-name or $MYSQL_CLUSTER_NAME")
+	}
+	if len(s.ClusterName) == 0 {
+		return fmt.Errorf("failed to detect hostname. Set --hostname")
+	}
+	return nil
+}
---
+++ pkg/options/operator/options.go
@@ -0,0 +1,129 @@
+// Copyright 2018 Oracle and/or its affiliates. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operator
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	mysqlServer = "mysql/mysql-server"
+	mysqlAgent  = "iad.ocir.io/oracle/mysql-agent"
+)
+
+// Images is the configuration of required MySQLOperator images. Remember to configure the appropriate
+// credentials for the target repositories.
+type Images struct {
+	MySQLServerImage string `yaml:"mysqlServer"`
+	MySQLAgentImage  string `yaml:"mysqlAgent"`
+}
+
+// MySQLOperatorOpts holds the options for the MySQLOperator.
+type MySQLOperatorOpts struct {
+	// KubeConfig is the path to a kubeconfig file, specifying how to connect to
+	// the API server.
+	KubeConfig string `yaml:"kubeconfig"`
+
+	// Master is the address of the Kubernetes API server (overrides any value
+	// in kubeconfig).
+	Master string `yaml:"master"`
+
+	// Namespace is the (optional) namespace in which the MySQL operator will
+	// manage MySQL Clusters. Defaults to metav1.NamespaceAll.
+	Namespace string `yaml:"namespace"`
+
+	// Hostname of the pod the operator is running in.
+	Hostname string `yaml:"hostname"`
+
+	// Images defines the 'mysql-server' and 'mysql-agent' images to use.
+	Images Images `yaml:"images"`
+
+	// minResyncPeriod is the resync period in reflectors; will be random
+	// between minResyncPeriod and 2*minResyncPeriod.
+	MinResyncPeriod metav1.Duration `yaml:"minResyncPeriod"`
+}
+
+// MySQLOperatorOpts will create a new MySQLOperatorOpts. If a valid
+// config file is specified and exists, it will be used to initialise the
+// server. Otherwise, a default server will be created.
+//
+// The values specified by either default may later be customised and overidden
+// by user specified commandline parameters.
+func NewMySQLOperatorOpts(filePath string) (*MySQLOperatorOpts, error) {
+	var config MySQLOperatorOpts
+	yamlPath, err := filepath.Abs(filePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to determine MySQLOperator configuration absolute path: '%s'", filePath)
+	}
+	if _, err := os.Stat(filePath); err == nil {
+		yamlFile, err := ioutil.ReadFile(yamlPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read MySQLOperator configuration: '%s'", filePath)
+		}
+		err = yaml.Unmarshal(yamlFile, &config)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse MySQLOperator configuration: '%s'", filePath)
+		}
+	} else {
+		config = MySQLOperatorOpts{}
+	}
+	config.EnsureDefaults()
+	return &config, nil
+}
+
+// EnsureDefaults provides a default configuration when required values have
+// not been set.
+func (s *MySQLOperatorOpts) EnsureDefaults() {
+	if s.Hostname == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			glog.Fatalf("Failed to get the hostname: %v", err)
+		}
+		s.Hostname = hostname
+	}
+	if &s.Images == nil {
+		s.Images = Images{}
+	}
+	if s.Images.MySQLServerImage == "" {
+		s.Images.MySQLServerImage = mysqlServer
+	}
+	if s.Images.MySQLAgentImage == "" {
+		s.Images.MySQLAgentImage = mysqlAgent
+	}
+	if s.MinResyncPeriod.Duration <= 0 {
+		s.MinResyncPeriod = metav1.Duration{Duration: 12 * time.Hour}
+	}
+}
+
+// AddFlags adds the mysql-operator flags to a given FlagSet.
+func (s *MySQLOperatorOpts) AddFlags(fs *pflag.FlagSet) *pflag.FlagSet {
+	fs.StringVar(&s.KubeConfig, "kubeconfig", s.KubeConfig, "Path to Kubeconfig file with authorization and master location information.")
+	fs.StringVar(&s.Master, "master", s.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig).")
+	fs.StringVar(&s.Namespace, "namespace", metav1.NamespaceAll, "The namespace for which the MySQL operator manages MySQL clusters. Defaults to all.")
+	fs.StringVar(&s.Images.MySQLServerImage, "mysql-server-image", s.Images.MySQLServerImage, "The name of the target 'mysql-server' image. Defaults to: mysql/mysql-server.")
+	fs.StringVar(&s.Images.MySQLAgentImage, "mysql-agent-image", s.Images.MySQLAgentImage, "The name of the target 'mysql-agent' image. Defaults to: iad.ocir.io/oracle/mysql-agent.")
+	fs.DurationVar(&s.MinResyncPeriod.Duration, "min-resync-period", s.MinResyncPeriod.Duration, "The resync period in reflectors will be random between MinResyncPeriod and 2*MinResyncPeriod.")
+	return fs
+}
