293bad9bea7e9c10e57d3ffc85b9c09a3e0f9265
Alan Tang <alantang888@gmail.com>
2019-03-19 16:56:38 +0800

    Keep mysql readonly after exit group unintentionally
    Since mysql version >= 8.0.12, <= 8.0.15 changed default behavior
    to `ABORT_SERVER`. Which will shutdown the server after exit group
    unintentionally. It should better change server to `READ_ONLY` instead
    of exit.
    
    Future version MySQL also change this behavior back to `READ_ONLY`
    https://dev.mysql.com/doc/refman/8.0/en/group-replication-options.html#sysvar_group_replication_exit_state_action
    
    Signed-off-by: Alan Tang <alantang888@gmail.com>

--- pkg/resources/statefulsets/statefulset.go
+++ pkg/resources/statefulsets/statefulset.go
@@ -172,6 +172,7 @@ func mysqlServerContainer(cluster *v1alpha1.Cluster, mysqlServerImage string, ro
 		"--master-info-repository=TABLE",
 		"--relay-log-info-repository=TABLE",
 		"--transaction-write-set-extraction=XXHASH64",
+		"--group-replication-exit-state-action=READ_ONLY",
 		fmt.Sprintf("--relay-log=%s-${index}-relay-bin", cluster.Name),
 		fmt.Sprintf("--report-host=\"%[1]s-${index}.%[1]s\"", cluster.Name),
 		"--log-error-verbosity=3",
